9e40730a17be97a629017ffb0cff53cb62241f75
Atique <11529740+atqatq@users.noreply.github.com>
2015-06-02 16:30:00 +0000

    Add holt_winters (forecasting)
    Triple Exponential Smoothing (Holt-Winters) with seasonality.

--- supplycm/forecasting/__init__.py
+++ supplycm/forecasting/__init__.py
@@ -3,3 +3,4 @@ from .simple_moving_average import simple_moving_average
 from .weighted_moving_average import weighted_moving_average
 from .single_exponential_smoothing import single_exponential_smoothing
 from .holt_linear_trend import holt_linear_trend
+from .holt_winters import holt_winters
