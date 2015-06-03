348abcb52f00b53d0964de8e346dea3ecefac1b9
Atique <11529740+atqatq@users.noreply.github.com>
2015-06-03 10:00:00 +0000

    Add naive_forecast (forecasting)
    Naive forecast: F(t+1) = X(t).

--- supplycm/forecasting/__init__.py
+++ supplycm/forecasting/__init__.py
@@ -4,3 +4,4 @@ from .weighted_moving_average import weighted_moving_average
 from .single_exponential_smoothing import single_exponential_smoothing
 from .holt_linear_trend import holt_linear_trend
 from .holt_winters import holt_winters
+from .naive_forecast import naive_forecast
