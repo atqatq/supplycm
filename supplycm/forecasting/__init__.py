a2b868411e7e34e1b91f36c3f8188ae3a4ffdaa5
Atique <11529740+atqatq@users.noreply.github.com>
2015-12-23 11:08:32 +0000

    Add bates_granger_combination (forecasting)
    Bates-Granger forecast combination weights.

--- supplycm/forecasting/__init__.py
+++ supplycm/forecasting/__init__.py
@@ -31,3 +31,4 @@ from .browns_triple_exponential import browns_triple_exponential
 from .pegels_classification import pegels_classification
 from .bottom_up_reconciliation import bottom_up_reconciliation
 from .top_down_reconciliation import top_down_reconciliation
+from .bates_granger_combination import bates_granger_combination
