5ae82ae553234507e230a5d2473eedc35821d51a
Atique <11529740+atqatq@users.noreply.github.com>
2016-04-11 09:55:40 +0000

    Add doubling_seasonal_smoothing (forecasting)
    Smith & Achuthan's doubling seasonal smoothing (3-parameter).

--- supplycm/forecasting/__init__.py
+++ supplycm/forecasting/__init__.py
@@ -41,3 +41,4 @@ from .adf_test import adf_test
 from .ljung_box_test import ljung_box_test
 from .hurst_exponent import hurst_exponent
 from .seasonal_indices import seasonal_indices
+from .doubling_seasonal_smoothing import doubling_seasonal_smoothing
