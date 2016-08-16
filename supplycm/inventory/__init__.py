2e403eb9f03501b7453c848bc4a5b9b9b83f009a
Atique <11529740+atqatq@users.noreply.github.com>
2016-08-16 14:13:59 +0000

    Add eoq_quantity_discount (inventory)
    EOQ with all-units quantity discounts.

--- supplycm/inventory/__init__.py
+++ supplycm/inventory/__init__.py
@@ -2,3 +2,4 @@
 from .economic_order_quantity import economic_order_quantity
 from .economic_production_quantity import economic_production_quantity
 from .eoq_with_backorders import eoq_with_backorders
+from .eoq_quantity_discount import eoq_quantity_discount
