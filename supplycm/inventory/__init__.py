811bfb6324087068d3a2bde0ec1e26fafb483dc4
Atique <11529740+atqatq@users.noreply.github.com>
2016-08-25 19:44:12 +0000

    Add newsvendor_model (inventory)
    Newsvendor model for single-period inventory.

--- supplycm/inventory/__init__.py
+++ supplycm/inventory/__init__.py
@@ -3,3 +3,4 @@ from .economic_order_quantity import economic_order_quantity
 from .economic_production_quantity import economic_production_quantity
 from .eoq_with_backorders import eoq_with_backorders
 from .eoq_quantity_discount import eoq_quantity_discount
+from .newsvendor_model import newsvendor_model
