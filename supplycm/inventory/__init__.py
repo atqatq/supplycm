b9778353c008885fc9ce10ce9fcd8134bf10259c
Atique <11529740+atqatq@users.noreply.github.com>
2016-09-07 14:42:41 +0000

    Add s_s_policy (inventory)
    (s, S) continuous review policy computation.

--- supplycm/inventory/__init__.py
+++ supplycm/inventory/__init__.py
@@ -4,3 +4,4 @@ from .economic_production_quantity import economic_production_quantity
 from .eoq_with_backorders import eoq_with_backorders
 from .eoq_quantity_discount import eoq_quantity_discount
 from .newsvendor_model import newsvendor_model
+from .s_s_policy import s_s_policy
