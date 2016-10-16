98c50d84c067af2223d78c99f063bcdff1eac640
Atique <11529740+atqatq@users.noreply.github.com>
2016-10-16 19:40:03 +0000

    Add abc_analysis (inventory)
    ABC (Pareto) analysis of inventory items.

--- supplycm/inventory/__init__.py
+++ supplycm/inventory/__init__.py
@@ -8,3 +8,4 @@ from .s_s_policy import s_s_policy
 from .r_q_policy import r_q_policy
 from .base_stock_policy import base_stock_policy
 from .periodic_review_policy import periodic_review_policy
+from .abc_analysis import abc_analysis
