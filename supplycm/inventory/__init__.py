d13705eeb47247829db3ba22462485b2244210b9
Atique <11529740+atqatq@users.noreply.github.com>
2016-12-13 15:51:26 +0000

    Add reorder_point (inventory)
    Reorder point calculation with safety stock.

--- supplycm/inventory/__init__.py
+++ supplycm/inventory/__init__.py
@@ -13,3 +13,4 @@ from .xyz_analysis import xyz_analysis
 from .abc_xyz_matrix import abc_xyz_matrix
 from .safety_stock_normal import safety_stock_normal
 from .safety_stock_with_lead_time_var import safety_stock_with_lead_time_var
+from .reorder_point import reorder_point
