0e3ac88ce6e61ea97d2ee7adf59f297d6e0518e0
Atique <11529740+atqatq@users.noreply.github.com>
2016-12-23 09:42:27 +0000

    Add fill_rate_calculation (inventory)
    Fill rate (service level) calculation.

--- supplycm/inventory/__init__.py
+++ supplycm/inventory/__init__.py
@@ -14,3 +14,4 @@ from .abc_xyz_matrix import abc_xyz_matrix
 from .safety_stock_normal import safety_stock_normal
 from .safety_stock_with_lead_time_var import safety_stock_with_lead_time_var
 from .reorder_point import reorder_point
+from .fill_rate_calculation import fill_rate_calculation
