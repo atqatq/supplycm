133e9906486ce8d318fc4122bf6e6033af865c3a
Atique <11529740+atqatq@users.noreply.github.com>
2017-01-25 14:46:21 +0000

    Add days_of_supply (inventory)
    Days of Supply (DOS) inventory metric.

--- supplycm/inventory/__init__.py
+++ supplycm/inventory/__init__.py
@@ -17,3 +17,4 @@ from .reorder_point import reorder_point
 from .fill_rate_calculation import fill_rate_calculation
 from .cycle_service_level import cycle_service_level
 from .inventory_turnover_ratio import inventory_turnover_ratio
+from .days_of_supply import days_of_supply
