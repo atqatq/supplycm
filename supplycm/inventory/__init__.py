0507e641a39a35f7c3f52a7c78961793d550e7a9
Atique <11529740+atqatq@users.noreply.github.com>
2017-11-26 17:06:04 +0000

    Add inventory_position (inventory)
    Inventory position = on-hand + on-order - backorders.

--- supplycm/inventory/__init__.py
+++ supplycm/inventory/__init__.py
@@ -44,3 +44,4 @@ from .spare_parts_sde import spare_parts_sde
 from .joint_replenishment import joint_replenishment
 from .vendor_managed_inventory import vendor_managed_inventory
 from .demand_during_lead_time import demand_during_lead_time
+from .inventory_position import inventory_position
