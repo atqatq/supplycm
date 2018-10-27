7e116666d836193277aeb787387e53188919985d
Atique <11529740+atqatq@users.noreply.github.com>
2018-10-27 15:22:19 +0000

    Add descriptive_stats (statistics)
    Compute descriptive statistics (mean, median, std, min, max, quartiles).

--- supplycm/statistics/__init__.py
+++ supplycm/statistics/__init__.py
@@ -14,3 +14,4 @@ from .forecast_value_added import forecast_value_added
 from .percent_bias import percent_bias
 from .coefficient_of_variation import coefficient_of_variation
 from .confidence_interval_mean import confidence_interval_mean
+from .descriptive_stats import descriptive_stats
