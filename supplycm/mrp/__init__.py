f083146c7d5a5845f8b32c5c6bb7e37e041ca293
Atique <11529740+atqatq@users.noreply.github.com>
2022-09-30 19:38:30 +0000

    Add phantom_bom_handling (mrp)
    Handle phantom items in BOM (skip-level processing).

--- supplycm/mrp/__init__.py
+++ supplycm/mrp/__init__.py
@@ -3,3 +3,4 @@ from .bom_explosion import bom_explosion
 from .mrp_calculation import mrp_calculation
 from .low_level_coding import low_level_coding
 from .where_used_query import where_used_query
+from .phantom_bom_handling import phantom_bom_handling
