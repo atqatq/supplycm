2a97476ea1f0d907a0cacd3814fe9ee0a61582bb
Atique <11529740+atqatq@users.noreply.github.com>
2022-11-02 12:50:50 +0000

    Add available_to_promise (mrp)
    Available-To-Promise (ATP) calculation.

--- supplycm/mrp/__init__.py
+++ supplycm/mrp/__init__.py
@@ -6,3 +6,4 @@ from .where_used_query import where_used_query
 from .phantom_bom_handling import phantom_bom_handling
 from .modular_bom import modular_bom
 from .planning_bom import planning_bom
+from .available_to_promise import available_to_promise
