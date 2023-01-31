880d05cf734464105b80fe9fe533165bf1dcd05f
Atique <11529740+atqatq@users.noreply.github.com>
2023-01-31 09:38:02 +0000

    Add safety_lead_time (mrp)
    Add safety lead time to standard lead time.

--- supplycm/mrp/__init__.py
+++ supplycm/mrp/__init__.py
@@ -14,3 +14,4 @@ from .master_production_schedule import master_production_schedule
 from .demand_time_fence import demand_time_fence
 from .planning_time_fence import planning_time_fence
 from .lead_time_offsetting import lead_time_offsetting
+from .safety_lead_time import safety_lead_time
