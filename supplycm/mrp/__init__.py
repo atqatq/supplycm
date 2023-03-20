65d96328a03e3f901b79ce24a5898f6caac769b6
Atique <11529740+atqatq@users.noreply.github.com>
2023-03-20 18:52:17 +0000

    Add lot_size_rule_poq (mrp)
    Period Order Quantity (POQ) lot sizing.

--- supplycm/mrp/__init__.py
+++ supplycm/mrp/__init__.py
@@ -18,3 +18,4 @@ from .safety_lead_time import safety_lead_time
 from .shrinkage_factor import shrinkage_factor
 from .lot_size_rule_l4l import lot_size_rule_l4l
 from .lot_size_rule_foq import lot_size_rule_foq
+from .lot_size_rule_poq import lot_size_rule_poq
