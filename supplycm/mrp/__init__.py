2cb60c45af3b036aad398ff16d0748b708f4ca3e
Atique <11529740+atqatq@users.noreply.github.com>
2023-03-29 20:48:11 +0000

    Add lot_size_rule_epr (mrp)
    Economic Part Period (EPP) lot sizing.

--- supplycm/mrp/__init__.py
+++ supplycm/mrp/__init__.py
@@ -19,3 +19,4 @@ from .shrinkage_factor import shrinkage_factor
 from .lot_size_rule_l4l import lot_size_rule_l4l
 from .lot_size_rule_foq import lot_size_rule_foq
 from .lot_size_rule_poq import lot_size_rule_poq
+from .lot_size_rule_epr import lot_size_rule_epr
