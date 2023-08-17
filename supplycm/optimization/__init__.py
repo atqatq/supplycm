ebc56adca84f8708caf263411865e96ba2d4a963
Atique <11529740+atqatq@users.noreply.github.com>
2023-08-17 12:35:08 +0000

    Add simulated_annealing (optimization)
    Simulated annealing metaheuristic.

--- supplycm/optimization/__init__.py
+++ supplycm/optimization/__init__.py
@@ -2,3 +2,4 @@
 from .simplex_method import simplex_method
 from .branch_and_bound import branch_and_bound
 from .genetic_algorithm import genetic_algorithm
+from .simulated_annealing import simulated_annealing
