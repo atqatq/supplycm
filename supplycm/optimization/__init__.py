746620df80882a79b28a55ddb5f9bbd525c8de99
Atique <11529740+atqatq@users.noreply.github.com>
2023-09-03 15:56:44 +0000

    Add tabu_search (optimization)
    Tabu search metaheuristic.

--- supplycm/optimization/__init__.py
+++ supplycm/optimization/__init__.py
@@ -3,3 +3,4 @@ from .simplex_method import simplex_method
 from .branch_and_bound import branch_and_bound
 from .genetic_algorithm import genetic_algorithm
 from .simulated_annealing import simulated_annealing
+from .tabu_search import tabu_search
