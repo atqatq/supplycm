81907d406de5b5a2da619ec8f9addee55b48edb5
Atique <11529740+atqatq@users.noreply.github.com>
2024-01-14 12:52:00 +0000

    Add merge_sort (optimization)
    Merge sort (used as utility).

---
+++ supplycm/optimization/merge_sort.py
@@ -0,0 +1,30 @@
+"""Merge sort (used as utility)."""
+from typing import List, TypeVar
+
+T = TypeVar('T')
+
+
+def merge_sort(arr: List[T]) -> List[T]:
+    """Sort via merge sort.
+
+    Example:
+        >>> merge_sort([3, 1, 4, 1, 5, 9, 2, 6])
+        [1, 1, 2, 3, 4, 5, 6, 9]
+    """
+    if len(arr) <= 1:
+        return list(arr)
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
