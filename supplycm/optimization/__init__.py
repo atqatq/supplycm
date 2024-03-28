b3b5ecaff2254b6eb8c1efed3d9dce2a5870aa58
Atique <11529740+atqatq@users.noreply.github.com>
2024-03-28 20:26:02 +0000

    Add bin_packing_first_fit_decreasing (optimization)
    First-fit decreasing bin packing.

--- supplycm/optimization/__init__.py
+++ supplycm/optimization/__init__.py
@@ -22,3 +22,4 @@ from .heap_sort import heap_sort
 from .set_cover_greedy import set_cover_greedy
 from .bin_packing_first_fit import bin_packing_first_fit
 from .bin_packing_best_fit import bin_packing_best_fit
+from .bin_packing_first_fit_decreasing import bin_packing_first_fit_decreasing
