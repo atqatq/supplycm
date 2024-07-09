5ac015d1ec0bac2b7486c53a7d56599ac219992d
Atique <11529740+atqatq@users.noreply.github.com>
2024-07-09 18:05:54 +0000

    Add data_envelopment_analysis (supplier)
    Data Envelopment Analysis (CCR ratio model).

--- supplycm/supplier/__init__.py
+++ supplycm/supplier/__init__.py
@@ -1,3 +1,4 @@
 """supplycm.supplier - Supplier algorithms."""
 from .ahp_supplier_selection import ahp_supplier_selection
 from .topsis import topsis
+from .data_envelopment_analysis import data_envelopment_analysis
