f5bbf7999988e159167d21f2f6c2e836b36d32ec
Atique <11529740+atqatq@users.noreply.github.com>
2024-07-29 13:40:45 +0000

    Add supplier_risk_score (supplier)
    Composite supplier risk score.

--- supplycm/supplier/__init__.py
+++ supplycm/supplier/__init__.py
@@ -3,3 +3,4 @@ from .ahp_supplier_selection import ahp_supplier_selection
 from .topsis import topsis
 from .data_envelopment_analysis import data_envelopment_analysis
 from .weighted_point_method import weighted_point_method
+from .supplier_risk_score import supplier_risk_score
