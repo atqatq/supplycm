9017109badd889ebcb68e06121e041544c666cc5
Atique <11529740+atqatq@users.noreply.github.com>
2024-08-13 15:55:34 +0000

    Add supplier_evaluation_matrix (supplier)
    Build supplier evaluation matrix.

--- supplycm/supplier/__init__.py
+++ supplycm/supplier/__init__.py
@@ -4,3 +4,4 @@ from .topsis import topsis
 from .data_envelopment_analysis import data_envelopment_analysis
 from .weighted_point_method import weighted_point_method
 from .supplier_risk_score import supplier_risk_score
+from .supplier_evaluation_matrix import supplier_evaluation_matrix
