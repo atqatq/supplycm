1e079894b37839f829f4a928e7b8032a19928ea9
Atique <11529740+atqatq@users.noreply.github.com>
2025-07-21 11:08:50 +0000

    Add cross_dock_scheduling (warehouse)
    Cross-docking door assignment.

--- supplycm/warehouse/__init__.py
+++ supplycm/warehouse/__init__.py
@@ -5,3 +5,4 @@ from .traveling_salesman_picking import traveling_salesman_picking
 from .s_shape_routing import s_shape_routing
 from .return_routing import return_routing
 from .pallet_building import pallet_building
+from .cross_dock_scheduling import cross_dock_scheduling
