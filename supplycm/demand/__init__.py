b181ab87746dd3e63a7013ac9d1831347bfbfd70
Atique <11529740+atqatq@users.noreply.github.com>
2025-09-30 18:46:43 +0000

    Add demand_class_abc_xyz (demand)
    Combine ABC value with XYZ variability classification.

--- supplycm/demand/__init__.py
+++ supplycm/demand/__init__.py
@@ -1,3 +1,4 @@
 """supplycm.demand - Demand algorithms."""
 from .demand_aggregation import demand_aggregation
 from .demand_disaggregation import demand_disaggregation
+from .demand_class_abc_xyz import demand_class_abc_xyz
